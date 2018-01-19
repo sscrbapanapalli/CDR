327a3b8287047764eac90bd55ffcdf41234686ea
SSC.BAMMU <SSC.BAMMU@INCHEDTDM4173.INCHESSC.COM>
2018-01-19 18:12:38 +0530

    changes for aop log

--- src/main/java/com/cmacgm/cdrserver/CdrApplication.java
+++ src/main/java/com/cmacgm/cdrserver/CdrApplication.java
@@ -1,11 +1,15 @@
 package com.cmacgm.cdrserver;
 
+import org.aspectj.lang.annotation.Aspect;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -16,6 +20,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableJpaAuditing
 @Configuration
 @EnableJpaRepositories
+@EnableAspectJAutoProxy
+@EnableAutoConfiguration
+@ComponentScan(	    basePackages = {"com.cmacgm.*"}, includeFilters = {@ComponentScan.Filter(Aspect.class)})
 public class CdrApplication  extends SpringBootServletInitializer  {
 
 	
